6e330d5cbf88026c00523fb3c305d0b58ea8a742
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-12-22 11:09:44 +0100

    Remove unnecessary commands when publishing documentation

--- ci/publish-documentation-to-github-pages.sh
+++ ci/publish-documentation-to-github-pages.sh
@@ -7,12 +7,6 @@ MESSAGE=$(git log -1 --pretty=%B)
 
 RELEASE_VERSION=$(cat pom.xml | grep -oPm1 "(?<=<version>)[^<]+")
 
-git checkout -- .mvn/maven.config
-
-# Concourse does shallow clones, so need the next 2 commands to have the gh-pages branch
-git remote set-branches origin 'gh-pages'
-git fetch -v
-
 git checkout gh-pages
 mkdir -p $RELEASE_VERSION/htmlsingle
 cp target/generated-docs/index.html $RELEASE_VERSION/htmlsingle
