4a8ce95946e83eef22c41bfa7e2a938d5e991995
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-12-22 11:15:20 +0100

    Add Git commands to get gh-pages branch

--- ci/publish-documentation-to-github-pages.sh
+++ ci/publish-documentation-to-github-pages.sh
@@ -7,6 +7,10 @@ MESSAGE=$(git log -1 --pretty=%B)
 
 RELEASE_VERSION=$(cat pom.xml | grep -oPm1 "(?<=<version>)[^<]+")
 
+# GHA does shallow clones, so need the next 2 commands to have the gh-pages branch
+git remote set-branches origin 'gh-pages'
+git fetch -v
+
 git checkout gh-pages
 mkdir -p $RELEASE_VERSION/htmlsingle
 cp target/generated-docs/index.html $RELEASE_VERSION/htmlsingle
